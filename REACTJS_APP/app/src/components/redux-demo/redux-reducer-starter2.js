70fa6ab138aba688a0093dc2ecb779c9c8e29c5b
hemashivakumar <hemashivakumar99@gmail.com>
2022-04-12 19:47:43 +0530

    commit

---
+++ REACTJS_APP/app/src/components/redux-demo/redux-reducer-starter2.js
@@ -0,0 +1,29 @@
+const ReduxStarter = () =>{
+    var iniPlayers = [{
+        name: "Shreyas",
+        age: 25,
+        awards: ["State","National"]
+    },
+    {
+        name: "Kay",
+        age: 26,
+        awards: ["State","National"]
+    },
+    {
+        name: "Cunny",
+        age: 27,
+        awards: ["State","National"]
+    }];
+
+    
+
+
+
+
+    return(
+        <div><h2>*****************Redux  starter demo******************</h2></div>
+
+    )
+}
+
+export default ReduxStarter;
